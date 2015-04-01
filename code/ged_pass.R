8173024d534e29ab509a19245feaf92fd27b62da
tonyfujs <tonyfujs@gmail.com>
2015-03-31 20:22:46 -0400

    added ged_pass() function

---
+++ code/ged_pass.R
@@ -0,0 +1,23 @@
+#' ged_pass()
+#'
+#' This function returns the number of participants who obtained their GED. It takes one dataframe as input. The data comes from the following ETO Results report: "[Admin] raw_enrollment_report".
+#' @param enroll_data dataframe: a dataframe containing enrollment data."[Admin] raw_enrollment_report"
+#' @param eto_programs character vector:  a vector of character containing the name of ETO programs to keep for analysis.
+#' @return numeric
+#' @export
+#' @examples
+#' enroll <- laycUtils::load_txt('./my_data_folder/enrollment.txt')
+#' enroll <- laycUtils::format_data(enroll)
+#' 
+#' ged_pass(enroll_data = enroll)
+
+ged_pass <- function(enroll_data, eto_programs = c("ss - ged", 
+                                                   "pg - employment ged", 
+                                                   "dc - wise ged") {
+  enroll %>%
+  filter(program_name == 'ss - ged') %>%
+  filter(successfully_completed == 'yes') %>%
+  select(subject_id) %>%
+  distinct %>%
+  count
+}
